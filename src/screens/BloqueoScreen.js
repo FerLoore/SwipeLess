f09cd6a1c510953e1011deda0ef7cc1bf0d1aa0b
Fernando Lopez Orellana <digoferloorellana@gmail.com>
2026-08-09 22:56:57 -0600

    feat: se crea clase de bloqueoScreen

---
+++ src/screens/BloqueoScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const BloqueoScreen = () => {
+    return (
+        <View>
+            <Text>BloqueoScreen</Text>
+        </View>
+    )
+}
+
+export default BloqueoScreen
+
+const styles = StyleSheet.create({})
